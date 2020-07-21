dd9f73b21c8072bf0c366e0ab38e9cfb59c5635a
Cleme-coder <67659694+Cleme-coder@users.noreply.github.com>
2020-07-21 15:02:15 -0400

    start coding

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+blockyTalkyBLE.onStringReceived("show", function ({ stringValue }) {
+    if (stringValue == "F") {
+    	
+    }
 })
+function front () {
+	
+}
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Heart)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Happy)
+})
+led.enable(true)
+PCA9685.init(67, 0)
